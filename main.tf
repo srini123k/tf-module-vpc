e659ecf31b8b3157b65a51a352b51aa12f063791
Srini K <srinikdevops0131@gmail.com>
2023-03-19 17:14:53 -0700

    updating ./tf-module-vpc

--- main.tf
+++ main.tf
@@ -1,4 +1,7 @@
 resource "aws_vpc" "main" {
   cidr_block = var.vpc_cidr
-  tags = merge(var.tags,   { Name = "${var.env}-vpc" })
+  tags = merge(
+    var.tags,
+    { Name = "${var.env}-vpc" }
+  )
 }
