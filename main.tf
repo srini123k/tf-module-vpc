72879f453666c374787093dc220d280d9cc0b00c
Srini K <srinikdevops0131@gmail.com>
2023-03-19 17:26:15 -0700

    updating ./tf-module-vpc

--- main.tf
+++ main.tf
@@ -5,3 +5,17 @@ resource "aws_vpc" "main" {
     { Name = "${var.env}-vpc" }
   )
 }
+
+######Public Subnets #######
+
+resource "aws_subnet" "public_subnets" {
+  vpc_id = aws_vpc.main.id
+
+  for_each          = var.public_subnets
+  cidr_block        = each.value["cidr_block"]
+  availability_zone = each.value["availability_zone"]
+  tags = merge(
+    var.tags,
+    { Name = "${var.env}-${each.value["name"]}" }
+  )
+}
